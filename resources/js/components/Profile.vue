b242acec08b2b533a2c3cfb7b1e391c626cf283a
Simon Dániel Zoltán <badsimon@hotmail.hu>
2023-03-16 14:01:10 +0100

    add profile form validation

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -10,38 +10,53 @@
               <div class="col-md-6">
                 <label for="firstName">First Name</label>
                 <input
+                  id="firstName-input"
                   type="text"
                   v-model="user.firstName"
                   class="form-control"
                   placeholder="Example"
                 />
+                <div id="firstName-invalid" class="invalid-feedback"></div>
               </div>
               <div class="col-md-6">
                 <label for="lastName">Last Name</label>
                 <input
+                  id="lastName-input"
                   type="text"
                   v-model="user.lastName"
                   class="form-control"
                   placeholder="John"
                 />
+                <div id="lastName-invalid" class="invalid-feedback"></div>
               </div>
               <div class="col-12">
                 <label for="email">Email</label>
                 <input
+                  id="email-input"
                   type="email"
                   v-model="user.email"
                   class="form-control"
                   placeholder="example.john@cat.com"
                 />
+                <div id="email-invalid" class="invalid-feedback"></div>
               </div>
               <div class="col-md-3">
                 <label for="zipCode">Zip code</label>
                 <input
-                  type="number"
+                  id="zipCode-input"
+                  type="text"
+                  pattern="[0-9]{4}"
                   v-model="user.zipCode.zipCode"
+                  :class="{
+                    'is-valid': zipCodeInput.isValid,
+                    'is-invalid': zipCodeInput.isError,
+                  }"
                   class="form-control"
                   placeholder="1000"
                 />
+                <div id="zipCode-invalid" class="invalid-feedback">
+                  Please provide a valid zip.
+                </div>
               </div>
               <div class="col-md-4 offset-md-1">
                 <label for="city">City</label>
@@ -66,11 +81,13 @@
               <div class="col-12">
                 <label for="address">Address</label>
                 <input
+                  id="address-input"
                   type="text"
                   v-model="user.address"
                   class="form-control"
                   placeholder="Széchenyi tér 1"
                 />
+                <div id="address-invalid" class="invalid-feedback"></div>
               </div>
               <div class="col-12">
                 <button type="submit" class="btn btn-primary">Submit</button>
@@ -90,6 +107,10 @@ export default {
   name: "profile",
   data() {
     return {
+      zipCodeInput: {
+        isValid: false,
+        isError: false,
+      },
       user: {
         address: "",
         firstName: "",
@@ -129,7 +150,22 @@ export default {
             first_name: this.user.firstName,
             last_name: this.user.lastName,
           });
+        })
+        .catch(({ response }) => {
+          if (response.status === 422) {
+            this.showErrors(response.data);
+          } else {
+            console.error(response);
+          }
         });
+    },
+    showErrors: function (error) {
+      for (const [field, message] of Object.entries(error.errors)) {
+        const input = document.getElementById(field + "-input");
+        const invalid = document.getElementById(field + "-invalid");
+        invalid.innerHTML = message;
+        input.classList.add("is-invalid");
+      }
     },
   },
   created() {
@@ -139,19 +175,29 @@ export default {
   },
   watch: {
     "user.zipCode.zipCode": function (val) {
-      if (val && val.toString().length === 4) {
+      if (val && val.length === 4) {
         axios
           .get(`api/zip-code/${val}`)
           .then(({ data }) => {
             this.user.zipCode = data.data;
+            this.zipCodeInput.isError = false;
+            this.zipCodeInput.isValid = true;
           })
           .catch(({ response }) => {
             if (response.status === 404) {
-              // TODO ERROR handling
+              this.zipCodeInput.isError = true;
+              this.zipCodeInput.isValid = false;
+              this.user.zipCode.city = "";
+              this.user.zipCode.county = "";
             } else {
-              console.log($response);
+              console.error(response);
             }
           });
+      } else {
+        this.user.zipCode.city = "";
+        this.user.zipCode.county = "";
+        this.zipCodeInput.isError = false;
+        this.zipCodeInput.isValid = false;
       }
     },
   },
